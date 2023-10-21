20a23525707d1fd003888b690f7c423c0c7f0934
hedeks <kirill12hicko@mail.ru>
2023-10-21 18:00:09 +0300

    completed header without router only styles

--- ecommerce-shop/src/App.vue
+++ ecommerce-shop/src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <Header />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Header from './components/Layout/Header.vue';
+export default {
+  components: {
+    Header
+  },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style lang="scss">
+@import url('https://fonts.cdnfonts.com/css/satoshi');
+* {
+  box-sizing: border-box;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Satoshi', sans-serif;
+} 
+.container {
+  max-width: 1440px;
+  margin:auto; 
 }
 </style>
---
+++ ecommerce-shop/src/components/Layout/Header.vue
@@ -0,0 +1,113 @@
+<template>
+  <header class="header">
+    <div class="header-top">
+      <div class="header-top-search">
+        <img src="/svg/Search.svg" />
+      </div>
+      <a href="#" class="header-logo">Avion</a>
+      <div class="header-top-right">
+        <div class="header-top-right__cart">
+          <a href="#">
+            <img src="/svg/Vector.svg" />
+          </a>
+        </div>
+        <div class="header-top-right__user">
+          <a href="#">
+            <img src="/svg/User--avatar.svg" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="header-menu">
+      <a :href="el.path" v-for="(el, i) in menu" :key="i" class="header-menu__link"> {{ el.name }}</a>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          name: "Plant pots",
+          path: "/plant"
+        },
+        {
+          name: "Ceramics",
+          path: "/ceramics"
+        },
+        {
+          name: "Tables",
+          path: "/tables"
+        },
+        {
+          name: "Chairs",
+          path: "/chairs"
+        },
+        {
+          name: "Tableware",
+          path: "/tableware"
+        },
+        {
+          name: "Cutlery",
+          path: "/cutlery"
+        }
+      ]
+    };
+  }
+}
+</script>
+<style lang="scss" scoped>
+.header {
+  height: 132px;
+  background: #fff;
+
+  &-top {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 70px;
+    align-items: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    margin: 0 28px;
+
+    &-right {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      &__cart {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  &-logo {
+    color: #22202e;
+    font-size: 24px;
+    letter-spacing: 2px;
+    line-height: 150%;
+    text-decoration: none;
+    text-align: center;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &-menu {
+    height: 62px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &__link {
+      margin: 0 22px;
+      color: #726E8D;
+      text-decoration: none;
+      font-size: 16px;
+      &-hover {
+        text-decoration: underline; 
+      }
+    }
+  }
+}
+</style>
