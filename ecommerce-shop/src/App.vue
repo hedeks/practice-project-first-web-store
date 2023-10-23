4a0bc9f3e8d6f33f5f2d671df04a62e13b350e9f
hedeks <kirill12hicko@mail.ru>
2023-10-23 18:27:40 +0300

    added about brand block

--- ecommerce-shop/src/App.vue
+++ ecommerce-shop/src/App.vue
@@ -2,35 +2,45 @@
   <div class="container">
     <Header />
     <Hero />
+    <AboutBrand />
   </div>
 </template>
 
 <script>
+import AboutBrand from './components/Layout/AboutBrand.vue';
 import Header from './components/Layout/Header.vue';
 import Hero from './components/Layout/Hero.vue';
 
 export default {
   components: {
-    Header, Hero
+    Header, Hero,
+    AboutBrand
   },
 }
 </script>
 
 <style lang="scss">
 @import url('https://fonts.cdnfonts.com/css/satoshi');
+
 * {
   box-sizing: border-box;
 }
+
 body {
   margin: 0;
   padding: 0;
   font-family: 'Satoshi', sans-serif;
-} 
+  font-size: 16px;
+  color: #22202e;
+  font-weight: 400;
+  line-height: 150%;
+}
+
 img {
   max-width: 100%;
 }
+
 .container {
   max-width: 1440px;
-  margin:auto; 
-}
-</style>
+  margin: auto;
+}</style>
---
+++ ecommerce-shop/src/components/Layout/AboutBrand.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="about-brand">
+        <h3 class="about-brand__title">What makes our brand different</h3>
+        <div class="about-brand-elements">
+            <div class="about-brand-element">
+                <img src="/svg/Delivery.svg" alt="delivery" class="about-brand-element__img">
+                <span class="about-brand-element__title">Next day as standard</span>
+                <span class="about-brand-element__description">Order before 3pm and get your order the next day as
+                    standard</span>
+            </div>
+            <div class="about-brand-element">
+                <object type="image/svg+xml" data="/svg/Checkmark--outline.svg" class="about-brand-element__img">
+                    Your browser does not support SVG
+                </object>
+                <span class="about-brand-element__title">Made by true artisans</span>
+                <span class="about-brand-element__description">Handmade crafted goods made with real passion and
+                    craftmanship
+                </span>
+            </div>
+            <div class="about-brand-element">
+                <img src="/svg/Purchase.svg" alt="Purchase" class="about-brand-element__img">
+                <span class="about-brand-element__title">Unbeatable prices</span>
+                <span class="about-brand-element__description">For our materials and quality you won’t find better prices
+                    anywhere
+                </span>
+            </div>
+            <div class="about-brand-element">
+                <img src="/svg/Sprout.svg" alt="Sprout" class="about-brand-element__img">
+                <span class="about-brand-element__title">Recycled packaging</span>
+                <span class="about-brand-element__description">We use 100% recycled to ensure our footprint is more
+                    manageable</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.about-brand {
+    padding: 80px 0 130px 0;
+
+    &__title {
+        text-align: center;
+        font-size: 24px;
+        color: #2A254B;
+        margin: 0 0 84px 0;
+        font-weight: 400;
+        line-height: 140%;
+    }
+
+    &-elements {
+        display: flex;
+        justify-content: center;
+    }
+
+    &-element {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        background: #F9F9F9;
+        padding: 48px;
+        margin: 0 9px;
+        width: 305px;
+
+        &__img {
+            height: 24px;
+            fill: none;
+        }
+
+        &__title {
+            margin-top: 12px;
+            margin-bottom: 12px;
+            font-size: 20px;
+            line-height: 140%;
+            color: #2A254B;
+        }
+
+        &__description {
+            color: #2A254B;
+        }
+    }
+}
+</style>
